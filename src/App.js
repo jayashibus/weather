3276ff23ea54d056d134ca954050975abeee78c2
Jeya Shibu <shibu@Shibus-MacBook-Pro.local>
2022-07-20 17:24:35 +0530

    First update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [weather, setWeather] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState("Kovan");
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=4122a62214a91aa6e72874bd63075a3d`;
+
+  const searcLocationHandler = (event) => {
+    if (event.key === "Enter") {
+      fetchWeather();
+    }
+  };
+
+  const fetchWeather = async () => {
+    try {
+      const response = await fetch(apiUrl);
+      const weatherData = await response.json();
+      setWeather(weatherData);
+      setLoading(false);
+      console.log(weatherData);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loading ? (
+        <div className="app-wrap">
+          <header>
+            <input
+              type="text"
+              autocomplete="off"
+              className="search-box"
+              placeholder="Search for a city"
+              onChange={(event) => setLocation(event.target.value)}
+              onKeyPress={searcLocationHandler}
+            />
+          </header>
+
+          <main>
+            <section className="location">
+              <div className="city">{weather.name}, </div>
+              <div className="date">Tuesday 20 July</div>
+            </section>
+
+            <div className="current">
+              <div className="temp">
+                {weather.main ? weather.main.temp.toFixed() : null}
+                <span>°C</span>
+              </div>
+              <div className="weather">
+                {weather.weather ? weather.weather[0].main : null}
+              </div>
+              <div className="hi-low">
+                {weather.main ? (
+                  <p>
+                    {weather.main.temp_min}°C / {weather.main.temp_max}°C
+                  </p>
+                ) : null}
+              </div>
+            </div>
+          </main>
+        </div>
+      ) : (
+        <div className="app-wrap">Loading....</div>
+      )}
     </div>
   );
 }
